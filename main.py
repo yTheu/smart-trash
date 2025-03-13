287e63ba21a27a65d17092c4d1933a7fe2a7ae8e
Matheus <matheuscs0v7@gmail.com>
2025-03-13 13:13:32 -0300

    Modelo treinado v.1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt") # Carrega o modelo
+model = YOLO("../runs/detect/train17/weights/best.pt") # Carrega o modelo
 cap =cv2.VideoCapture(0) # Inicia a captura de vídeo
 
 while cap.isOpened():
     ret, frame = cap.read() # Se a captura estiver funcionando, lê o que vê nela
-    frame = cv2.resize(frame, (640, 480)) # Redimensionar para menor resolução e maior FPS
+    frame = cv2.resize(frame, (640, 480)) # Redimensionar - menor resolução e maior FPS
 
     if not ret:
         break
@@ -16,12 +16,12 @@ while cap.isOpened():
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0]) # Coordenadas
-            label = model.names[int(box.cls)] # Nomes
+            label = model.names[int(box.cls)] # Nomes (Person, Cellphone...)
             conf = float(box.conf) # Para selecionar apenas objetos que tem confiança do que é (Se tem certeza que ta vendo uma pedra...)
 
             if conf > 0.5: # Confiança maior que 50%
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, f"{label} {conf * 100:.1f}%", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.putText(frame, f"{label} {conf * 100:.1f}%", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
     
     cv2.imshow("Detecção", frame) # Roda a captura e detecção
 
