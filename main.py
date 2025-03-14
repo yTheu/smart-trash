5130c8c8b313b28bfc7974044e3d9bf3d7f6dd63
Matheus <matheuscs0v7@gmail.com>
2025-03-14 16:07:30 -0300

    Set classes (Plastic, Paper...)

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import cv2
 from ultralytics import YOLO
+from maping import material_mapping
 
-model = YOLO("../runs/detect/train17/weights/best.pt") # Carrega o modelo
-cap =cv2.VideoCapture(0) # Inicia a captura de vídeo
+# Carrega o modelo treinado
+model = YOLO("../runs/detect/train17/weights/best.pt")
+
+# Inicia a captura de vídeo pela webcam
+cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
-    ret, frame = cap.read() # Se a captura estiver funcionando, lê o que vê nela
-    frame = cv2.resize(frame, (640, 480)) # Redimensionar - menor resolução e maior FPS
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (640, 480))
 
     if not ret:
         break
 
-    results = model(frame) # Passa os dados da captura, lida pelo modelo, para os resultados
+    results = model(frame)
 
     for result in results:
         for box in result.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0]) # Coordenadas
-            label = model.names[int(box.cls)] # Nomes (Person, Cellphone...)
-            conf = float(box.conf) # Para selecionar apenas objetos que tem confiança do que é (Se tem certeza que ta vendo uma pedra...)
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            label = model.names[int(box.cls)]
+            material = material_mapping.get(label, "Outro")  # Pega o tipo de material
 
-            if conf > 0.5: # Confiança maior que 50%
+            conf = float(box.conf)
+            if conf > 0.5:
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, f"{label} {conf * 100:.1f}%", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-    
-    cv2.imshow("Detecção", frame) # Roda a captura e detecção
+                cv2.putText(frame, f"{material} {conf * 100:.1f}%", (x1, y1 - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)  # Texto preto
+
+    cv2.imshow("Detecção", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
